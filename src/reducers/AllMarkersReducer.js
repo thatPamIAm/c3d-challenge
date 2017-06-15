37131fc690a194958c6d7293614e994d46438bae
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-06-15 10:01:53 -0600

    Add reducer for STORE_COORDINATES and STORE_ALL_COORDINATES.

---
+++ src/reducers/AllMarkersReducer.js
@@ -0,0 +1,19 @@
+const initialState = {
+  data: [],
+};
+
+const Coordinates = (state = initialState, action) => {
+  switch (action.type) {
+    case 'STORE_ALL_COORDINATES':
+      // stores an array of coordinates to draw to map
+      return Object.assign({}, state, {
+        data: state.data.concat(action.data),
+      });
+    case 'STORE_COORDINATES':
+      return Object.assign({}, state, { data: action.data });
+    default:
+      return state;
+  }
+};
+
+export default Coordinates;
