b3800eaf1aab223c15b1310692c941e40d508c2f
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-06-15 09:29:39 -0600

    Update AllMarkers container with bindActionCreators and mapping state coords to props.

--- src/containers/AllMarkers.js
+++ src/containers/AllMarkers.js
@@ -1,14 +1,17 @@
 /*eslint-disable no-unused-vars*/
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import AllMarkers from '../components/AllMarkers'
+import { saveCoordinates, removeCoordinates } from '../actions/locationActions';
+
 
 const mapStateToProps = (state) => {
-  return { locations: state.Locations.data }
+  return { locations: state.Locations.data, coordinates: state.Coordinates.data  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return {}
+  return bindActionCreators({ saveCoordinates, removeCoordinates }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllMarkers);
