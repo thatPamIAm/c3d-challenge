2f4c16483d091ebdb3bb03474a43ffe0fd954061
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-06-15 10:02:35 -0600

    Update and add Coordinates to root reducer.

--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import Locations from './LocationsReducer';
+import Coordinates from './AllMarkersReducer';
 
 const RootReducer = combineReducers({
   Locations,
+  Coordinates
 });
 
 export default RootReducer;
