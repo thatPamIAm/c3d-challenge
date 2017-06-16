54d12fea321b5d44ecfce0a45c97b949482db6b8
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-06-15 22:05:21 -0600

    Add testCoordinates fake data helper.

---
+++ src/_tests/testCoordinates.js
@@ -0,0 +1,16 @@
+const testCoordinates = [
+  {
+    lat: 39.742043,
+    lng: -104.991531,
+  },
+  {
+    lat: 34.052235,
+    lng: -118.243683,
+  },
+  {
+    lat: 42.364506,
+    lng: -71.038887,
+  },
+];
+
+export default testCoordinates;
